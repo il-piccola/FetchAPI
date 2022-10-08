72fdb10cd46f60161b6ffad83264c635940fa239
il-piccola <il.piccola.estate@gmail.com>
2022-10-08 14:49:49 +0900

    20221008_01

--- FetchAPI/static/index.js
+++ FetchAPI/static/index.js
@@ -1,15 +1,16 @@
 const spinnerHTML = '<div class="spinner-border text-primary" style="width: 200px; height: 200px;" role="status"><span class="visually-hidden">Loading...</span></div>'
 const buttonHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>画像生成中…'
-function showImage(url) {
+function showImage(url, n) {
+    const tag = '#img' + n
     document.querySelector('#button').disabled = true;
     document.querySelector('#button').innerHTML = buttonHTML;
-    document.querySelector('#img').innerHTML = spinnerHTML;
+    document.querySelector(tag).innerHTML = spinnerHTML;
     fetch(url).then(response => {
         response.blob().then(blobResponse => {
             const fileUrl = URL.createObjectURL(blobResponse);
             document.querySelector('#button').disabled = false;
             document.querySelector('#button').innerHTML = '画像生成開始'
-            document.querySelector('#img').innerHTML = `<img src='${fileUrl}' width=300 height=300 />`;
+            document.querySelector(tag).innerHTML = `<img src='${fileUrl}' width=300 height=300 />`;
         })
     })
 }
