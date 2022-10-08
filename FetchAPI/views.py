36dcda1d962b4e1c3ed0644c3591e65cc167ec7a
il-piccola <il.piccola.estate@gmail.com>
2022-10-08 15:08:15 +0900

    20221008_01

--- FetchAPI/views.py
+++ FetchAPI/views.py
@@ -1,7 +1,5 @@
-import mimetypes
 import os
 import time
-from subprocess import Popen, PIPE
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
