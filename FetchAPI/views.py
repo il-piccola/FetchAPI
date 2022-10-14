ce7581ff727de427de740a7f3308754a9293f849
il-piccola <il.piccola.estate@gmail.com>
2022-10-14 13:44:14 +0900

    20221014_01

--- FetchAPI/views.py
+++ FetchAPI/views.py
@@ -9,10 +9,6 @@ from diffusers import LMSDiscreteScheduler
 from japanese_stable_diffusion import JapaneseStableDiffusionPipeline
 from .settings import *
 
-scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
-pipe = JapaneseStableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16, scheduler=scheduler, use_auth_token=os.environ['HF_TOKEN'])
-pipe = pipe.to(DEVICE)
-
 @csrf_exempt
 def index(request) :
     params = {
@@ -21,6 +17,11 @@ def index(request) :
     return render(request, 'FetchAPI/index.html', params)
 
 def img(request, n) :
+    scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
+    pipe = JapaneseStableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16, scheduler=scheduler, use_auth_token=os.environ['HF_TOKEN'])
+    # pipe = JapaneseStableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16, scheduler=scheduler, use_auth_token="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")
+    pipe = pipe.to(DEVICE)
+
     imgpath = os.path.join(IMGDIR, IMGLIST[n])
     with autocast(DEVICE):
         image = pipe(SENTENSE, guidance_scale=7.5)["sample"][0]
