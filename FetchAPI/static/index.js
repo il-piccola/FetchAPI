79f74acc560b8ed2336636a9147a23bbd48aaf48
il-piccola <il.piccola.estate@gmail.com>
2022-11-02 13:38:08 +0900

    20221102_01

--- FetchAPI/static/index.js
+++ FetchAPI/static/index.js
@@ -1,16 +1,21 @@
-const spinnerHTML = '<div class="spinner-border text-primary" style="width: 200px; height: 200px;" role="status"><span class="visually-hidden">Loading...</span></div>'
-const buttonHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>画像生成中…'
+function switchButton(disabled) {
+    document.querySelector('#button').disabled = disabled;
+    if (disabled == true) {
+        const buttonHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>画像生成中…'
+        document.querySelector('#button').innerHTML = buttonHTML;
+    } else {
+        document.querySelector('#button').innerHTML = '画像生成開始'
+    }
+}
 function showImage(url, n) {
     const tag = '#img' + n
-    document.querySelector('#button').disabled = true;
-    document.querySelector('#button').innerHTML = buttonHTML;
+    const spinnerHTML = '<div class="spinner-border text-primary" style="width: 200px; height: 200px;" role="status"><span class="visually-hidden">Loading...</span></div>'
     document.querySelector(tag).innerHTML = spinnerHTML;
     fetch(url).then(response => {
         response.blob().then(blobResponse => {
             const fileUrl = URL.createObjectURL(blobResponse);
-            document.querySelector('#button').disabled = false;
-            document.querySelector('#button').innerHTML = '画像生成開始'
             document.querySelector(tag).innerHTML = `<img src='${fileUrl}' width=300 height=300 />`;
+            switchButton(false)
         })
     })
 }
