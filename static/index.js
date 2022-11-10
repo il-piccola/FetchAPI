441706e472a36229ebd761b98a3a8493e48b48ec
il-piccola <il.piccola.estate@gmail.com>
2022-11-10 13:43:27 +0900

    20221110_01

--- static/index.js
+++ static/index.js
@@ -10,7 +10,7 @@ function switchButton(disabled) {
 const showImage = async(url, n) => {
     const spinnerHTML = '<div class="spinner-border text-primary" style="width: 200px; height: 200px;" role="status"><span class="visually-hidden">Loading...</span></div>';
     switchButton(true);
-    for (i==0; i<n; i++) {
+    for (i=0; i<n; i++) {
         const tag = '#img' + i;
         document.querySelector(tag).innerHTML = spinnerHTML;
         try {
