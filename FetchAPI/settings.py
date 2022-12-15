8b5f842c3e93b38d5523e5f20a6ae750788f0517
il-piccola <il.piccola.estate@gmail.com>
2022-12-15 14:09:34 +0900

    20221215_01

--- FetchAPI/settings.py
+++ FetchAPI/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'FetchAPI',
 ]
 
@@ -116,6 +117,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+SHELL_PLUS = 'ipython'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
